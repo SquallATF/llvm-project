5ba899df8ccc845054f9998e157b1e6a698aca18
Peiyuan Song <squallatf@gmail.com>
2020-01-20 22:00:58 +0800

    openwrt fix

--- llvm/include/llvm/TargetParser/Triple.h
+++ llvm/include/llvm/TargetParser/Triple.h
@@ -193,7 +193,8 @@ public:
     Mesa,
     SUSE,
     OpenEmbedded,
-    LastVendorType = OpenEmbedded
+    OpenWrt,
+    LastVendorType = OpenWrt
   };
   enum OSType {
     UnknownOS,
@@ -566,6 +567,10 @@ public:
   bool isTargetMachineMac() const {
     return isMacOSX() || (isOSDarwin() && (isSimulatorEnvironment() ||
                                            isMacCatalystEnvironment()));
+  }
+
+  bool isOpenWrt() const {
+    return getVendor() == Triple::OpenWrt;
   }
 
   bool isOSNetBSD() const {
