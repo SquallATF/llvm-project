39787e4d173c8546373cde27e785df9bbe843990
Peiyuan Song <squallatf@gmail.com>
2020-03-16 09:29:19 +0800

    msvc not support unwindlib

--- clang/lib/Driver/ToolChains/MSVC.h
+++ clang/lib/Driver/ToolChains/MSVC.h
@@ -119,6 +119,9 @@ public:
 
   void printVerboseInfo(raw_ostream &OS) const override;
 
+  UnwindLibType
+  GetUnwindLibType(const llvm::opt::ArgList &Args) const override;
+
   bool FoundMSVCInstall() const { return !VCToolChainPath.empty(); }
 
   void
